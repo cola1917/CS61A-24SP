b124b29d74fa9d75321677eb3158c5ab715478ed
cola1917 <wangjiangtao1997@gmail.com>
2024-04-17 20:23:56 +0800

    finished week12 hw07

---
+++ week12/hw07/hw07.scm
@@ -0,0 +1,21 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (cond 
+    ((= 1 exp) exp)
+    ((odd? exp) (* base (pow base (- exp 1))))
+    ((even? exp) (* (square base) (pow base (/ exp 2))))
+  )
+)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let ((y (repeatedly-cube (- n 1) x)))
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cdr (cdr s))))
